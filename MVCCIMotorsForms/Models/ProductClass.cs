95d63bd1085a7e6290cdd80f07eac1abc93354a2
Romelyn Ungab <romelyn.ungab@gmail.com>
2017-02-23 10:17:10 +1300

    Added  the Product Class ready for 3

---
+++ MVCCIMotorsForms/Models/ProductClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCIMotorsForms.Models
+{
+    public class ProductClass
+    {
+        [Display(Name = "Product ID ")]
+        public int ProductId { get; set; }
+        [Display(Name = "Product Name ")]
+        public string Name { get; set; }
+        [Display(Name = "Product Category Id ")]
+        public int CatagoryId { get; set; }
+        [Display(Name = "Product Price ")]
+        public decimal Price { get; set; }
+    }
+}
